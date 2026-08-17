8f69b25e48fae55e5237dc418941918157894e82
dinesh2692 <rnagadinesh18@gmail.com>
2026-08-17 18:46:30 +0530

    Add Spring Boot application

---
+++ backend/src/main/java/com/codecircle/CodeCircleApplication.java
@@ -0,0 +1,5 @@
+package com.codecircle;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class CodeCircleApplication { public static void main(String[] args){ SpringApplication.run(CodeCircleApplication.class,args); } }
