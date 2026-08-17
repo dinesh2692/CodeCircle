2266bb1b961d161a329b4cf5750170ea5b618708
dinesh2692 <rnagadinesh18@gmail.com>
2026-08-17 18:46:41 +0530

    Add user repository

---
+++ backend/src/main/java/com/codecircle/auth/UserRepository.java
@@ -0,0 +1,4 @@
+package com.codecircle.auth;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository extends JpaRepository<AppUser,Long>{ Optional<AppUser> findByUsername(String username); boolean existsByUsername(String username); }
